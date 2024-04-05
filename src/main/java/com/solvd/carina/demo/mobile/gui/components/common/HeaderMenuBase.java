f4a3fe797906dc83c707f4c6e9192fdf5564b366
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-04-05 12:35:16 -0300

    Add header menu common component

--- src/main/java/com/solvd/carina/demo/mobile/gui/components/common/HeaderMenuBase.java
+++ src/main/java/com/solvd/carina/demo/mobile/gui/components/common/HeaderMenuBase.java
@@ -2,12 +2,15 @@ package com.solvd.carina.demo.mobile.gui.components.common;
 
 import com.solvd.carina.demo.mobile.gui.pages.common.CartPageBase;
 import com.solvd.carina.demo.mobile.gui.pages.common.MenuPageBase;
-import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.webdriver.gui.AbstractUIObject;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 
 public abstract class HeaderMenuBase extends AbstractUIObject {
+    public HeaderMenuBase(WebDriver driver) {
+        super(driver);
+    }
+
     public HeaderMenuBase(WebDriver driver, SearchContext searchContext) {
         super(driver, searchContext);
     }
--- src/main/java/com/solvd/carina/demo/mobile/gui/components/ios/HeaderMenu.java
+++ src/main/java/com/solvd/carina/demo/mobile/gui/components/ios/HeaderMenu.java
@@ -3,30 +3,25 @@ package com.solvd.carina.demo.mobile.gui.components.ios;
 import com.solvd.carina.demo.mobile.gui.components.common.HeaderMenuBase;
 import com.solvd.carina.demo.mobile.gui.pages.common.CartPageBase;
 import com.solvd.carina.demo.mobile.gui.pages.common.MenuPageBase;
-import com.solvd.carina.demo.mobile.gui.pages.ios.CartPage;
-import com.zebrunner.carina.utils.factory.DeviceType;
-import com.zebrunner.carina.utils.mobile.IMobileUtils;
+import com.zebrunner.carina.utils.factory.ICustomTypePageFactory;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 
-public class HeaderMenu extends HeaderMenuBase{
+public class HeaderMenu extends HeaderMenuBase implements ICustomTypePageFactory {
     public HeaderMenu(WebDriver driver, SearchContext searchContext) {
         super(driver, searchContext);
     }
-    @ExtendedFindBy(iosPredicate = "name == 'test-Menu'")
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[`name == \"test-Menu\"`]")
     private ExtendedWebElement menuBtn;
-    @ExtendedFindBy(iosPredicate = "name == 'test-Cart'")
+    @ExtendedFindBy(iosClassChain = "**/XCUIElementTypeOther[`name == \"test-Cart\"`]")
     private ExtendedWebElement cartBtn;
-
-
-
 
     @Override
     public CartPageBase clickOnCartBtn() {
         cartBtn.click();
-        return ;
+        return initPage(getDriver(), CartPageBase.class);
     }
     @Override
     public MenuPageBase clickOnMenuBtn() {
